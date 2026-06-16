b5d41acd0799291e8a158dcec62348a860d242ad
“jennifer-onuogu” <jennyonuogu@outlook.com>
2026-06-16 20:53:23 +0100

    fix supabase keys

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
+const SUPABASE_URL = "https://fkllsjhagnfialcjjsxv.supabase.co"
+const SUPABASE_KEY = "sb_publishable_Fg_x_ME1_0TOvS9Bwk-LbA_7kcT2PXr"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
